ab69e4a903d853d7631fbe644e5fb56ac27c3f93
MfonAbasi Udobia <31806957+Mbrain1@users.noreply.github.com>
2022-02-27 20:44:39 +0100

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -2,14 +2,7 @@
 import { initializeApp } from "firebase/app";
 import {GoogleAuthProvider, getAuth} from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA1thS0zdqSZZ0Pw7Le6CZ0Z2h4GzNexHg",
-  authDomain: "uber-next-clone-b7062.firebaseapp.com",
-  projectId: "uber-next-clone-b7062",
-  storageBucket: "uber-next-clone-b7062.appspot.com",
-  messagingSenderId: "461025993201",
-  appId: "1:461025993201:web:d53a8f5da14ff1cefbe450"
-};
+const firebaseConfig = //Your fire base config comes here
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
